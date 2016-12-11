e3b6b0f1942730ef8883b56f6fa06b80b15719f1
Alex Kabbach <akabbach@gmail.com>
2016-12-11 10:12:37 +0900

    Refactoring utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,35 @@
+function containsWhiteSpace(string) {
+  return /\+/.test(string);
+}
+
+function isWhiteSpace(char) {
+  if (char.length === 1) {
+    return /\+/.test(char);
+  }
+}
+
+// A.B.C+D.E.F+G.H.I --> [A.B.C, D.E.F, G.H.I]
+// Check for invalid characters (regex, everything except . and +)
+// Check for invalid combinations: ++ +. .+ .start end.
+function toValenceArray(string) {
+  string = string.trim();
+  if (containsWhiteSpace(string)) {
+    const array = [];
+    let iterator = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      if (isWhiteSpace(string.charAt(i))) {
+        if (iterator !== i) {
+          array.push(string.substring(iterator, i));
+        }
+        iterator = i + 1;
+      }
+    }
+    array.push(string.substring(iterator, string.length));
+    return array;
+  }
+  return [string];
+}
+
 function containsPeriod(string) {
   return /\./.test(string);
 }
@@ -8,6 +40,7 @@ function isPeriod(char) {
   }
 }
 
+// TODO throw error if length of valenceArray is > 3
 // [A.B.C, D.E.F, G.H.I] --> [[A, B, C], [D, E, F], [G, H, I]]
 function toTokenArray(valenceArray) {
   const tokenArray = [];
@@ -33,5 +66,6 @@ function toTokenArray(valenceArray) {
 }
 
 export default {
+  toValenceArray,
   toTokenArray,
 };
