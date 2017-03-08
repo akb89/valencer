3472a794c573a8e327c7fc394bbd44fd41a1ef8d
Alex Kabbach <akabbach@gmail.com>
2017-03-08 15:48:40 +0100

    Ignoring modifs to config

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 import logger from './../logger/logger';
 
 const production = {
-  dbUri: 'mongodb://localhost:27017/noframenet16',
+  dbUri: 'mongodb://localhost:27017/noframenet15',
   port: 3030,
   logger: logger.info,
 };
