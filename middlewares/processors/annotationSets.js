fa6bc2370ff840eccd8b127fe1b41cec8a29c297
akb89 <akb@3azouz.net>
2017-06-20 18:32:07 +0200

    Removed populate

--- middlewares/processors/annotationSets.js
+++ middlewares/processors/annotationSets.js
@@ -4,7 +4,7 @@ const utils = require('./../../utils/utils');
 
 const logger = config.logger;
 
-async function getByNoPopulateVP(filteredPatternsIDs) {
+async function getAnnotationSets(filteredPatternsIDs) {
   // This is slightly faster than with mongoose
   return AnnotationSet.collection.aggregate([{
     $match: {
@@ -14,30 +14,10 @@ async function getByNoPopulateVP(filteredPatternsIDs) {
     } }]).toArray();
 }
 
-async function getByPopulateVP(filteredPatternsIDs) {
-  return AnnotationSet.find()
-                      .where('pattern')
-                      .in(filteredPatternsIDs)
-                      .populate([{
-                        path: 'pattern',
-                        populate: { path: 'valenceUnits' } }, {
-                        path: 'sentence' }, {
-                        path: 'lexUnit',
-                        populate: {
-                          path: 'frame',
-                          populate: [{
-                            path: 'lexUnits',
-                            select: 'name',
-                          }, { path: 'frameElements' }],
-                        },
-                        }, { path: 'labels' }]);
-}
-
 async function getByValencePattern(context, next) {
   const startTime = utils.getStartTime();
   logger.info(`Querying for all AnnotationSets with a valence pattern matching: '${context.query.vp}'`);
-  logger.info(`Returning populated documents: ${context.query.populate}`);
-  context.body = context.query.populate === 'true' ? await getByPopulateVP(context.valencer.results.filteredPatternsIDs) : await getByNoPopulateVP(context.valencer.results.filteredPatternsIDs);
+  context.body = await getAnnotationSets(context.valencer.results.filteredPatternsIDs);
   logger.debug(`${context.body.length} unique AnnotationSets retrieved from database in ${utils.getElapsedTime(startTime)}ms`);
   return next();
 }
