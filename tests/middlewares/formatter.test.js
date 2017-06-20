46e6052d498f9dbe5babf6a4b44a711ed710f0c9
akb89 <akb@3azouz.net>
2017-06-20 18:32:29 +0200

    Updated tests

--- tests/middlewares/formatter.test.js
+++ tests/middlewares/formatter.test.js
@@ -47,7 +47,19 @@ describe('formatter', () => {
   });
   it('#formatValencePatternToArrayOfArrayOfTokens should convert a full string formatted valence pattern to an array of array of tokens', () => {
     const next = () => {};
-    const context = { query: { vp: 'A.B.C+D.E.F+G.H.I' } };
+    const context = { query: { vp: 'A.B.C  D.E.F G.H.I' }, valencer: { query: { vp: {} } } };
+    formatter.formatValencePatternToArrayOfArrayOfTokens(context, next);
+    context.valencer.query.vp.formatted.should.deep.equal([['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]);
+  });
+  it('#formatValencePatternToArrayOfArrayOfTokens should convert a full string formatted valence pattern to an array of array of tokens', () => {
+    const next = () => {};
+    const context = { query: { vp: 'A.B.C  D.E.F G.H.I' }, valencer: { query: { vp: {} } } };
+    formatter.formatValencePatternToArrayOfArrayOfTokens(context, next);
+    context.valencer.query.vp.formatted.should.deep.equal([['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]);
+  });
+  it('#formatValencePatternToArrayOfArrayOfTokens should convert a full string formatted valence pattern to an array of array of tokens', () => {
+    const next = () => {};
+    const context = { query: { vp: 'A.B.C  D.E.F G.H.I' }, valencer: { query: { vp: {} } } };
     formatter.formatValencePatternToArrayOfArrayOfTokens(context, next);
     context.valencer.query.vp.formatted.should.deep.equal([['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]);
   });
