6eed799ad74e03e9b4e8decda19c635abac3487c
akb89 <akb@3azouz.net>
2017-09-11 12:17:29 +0200

    Added cytoscape format support

--- middlewares/processors/frames.js
+++ middlewares/processors/frames.js
@@ -5,24 +5,60 @@ const logger = config.logger;
 
 function getFramesWithFrameModel(Frame, LexUnit) {
   return async function getFrames(annotationSets, projections = {}, populations = []) {
-    console.log(JSON.stringify(annotationSets));
-    const lexUnits = await LexUnit.find().where('_id')
+    const lexunits = await LexUnit.find({}, 'frame').where('_id')
                                   .in(annotationSets.map(annoset => annoset.lexUnit));
-    const q = Frame.find({}, projections).where('_id')
-        .in(lexUnits.map(lexUnit => lexUnit.frame));
-    return populations.reduce((query, p) => query.populate(p), q);
+    const frames = Frame.find({}, projections).where('_id')
+        .in(lexunits.map(lexunit => lexunit.frame));
+    return populations.reduce((query, p) => query.populate(p), frames);
+  };
+}
+
+function appendme(lexunits) {
+  const final = [];
+  for (let i = 1; i < lexunits.length; i += 1) {
+    if (lexunits[0].frame === lexunits[i].frame) {
+      final.push({ data: { source: lexunits[0]._id, target: lexunits[i]._id } });
+    }
+  }
+}
+
+function getCytoFramesWithFrameModel(Frame, LexUnit, FrameRelation) {
+  return async function getCytoFrames(annotationSets) {
+    const lexunits = await LexUnit.find({}, 'frame').where('_id')
+                                  .in(annotationSets.map(annoset => annoset.lexUnit));
+    // console.log(lexunits);
+    const frames = await Frame.find({}, 'name').where('_id')
+        .in(lexunits.map(lexunit => lexunit.frame));
+    // console.log(frames);
+    const frameIDs = frames.map(frame => frame._id);
+    // const frameIDset = new Set(frameIDs);
+    const relations = await FrameRelation.find(
+      { $and: [{ subFrame: { $in: frameIDs } }, { supFrame: { $in: frameIDs } }] })
+      .populate('type');
+    // console.log(relations);
+    const cytoframes = frames.map(frame => ({ data: { id: frame._id, name: frame.name } }));
+    const cytorelations = relations.map(relation => ({
+      data: { id: relation._id, source: relation.supFrame, target: relation.subFrame } }));
+    return cytoframes.concat(cytorelations);
   };
 }
 
 async function getByAnnotationSets(context, next) {
   const startTime = utils.getStartTime();
   logger.info(`Querying for all Frames containing lexical units with a valence pattern matching: '${context.query.vp}'`);
-  console.log(context.valencer.results.annotationSets);
-  context.valencer.results.frames = await getFramesWithFrameModel(
-    context.valencer.models.Frame,
-    context.valencer.models.LexUnit)(context.valencer.results.annotationSets,
-                                     context.valencer.query.projections,
-                                     context.valencer.query.populations);
+  if (context.query.format === 'cytoscape') {
+    context.valencer.results.frames = await getCytoFramesWithFrameModel(
+      context.valencer.models.Frame, context.valencer.models.LexUnit,
+      context.valencer.models.FrameRelation)(
+        context.valencer.results.annotationSets);
+  } else {
+    context.valencer.results.frames = await getFramesWithFrameModel(
+      context.valencer.models.Frame,
+      context.valencer.models.LexUnit)(context.valencer.results.annotationSets,
+                                       context.valencer.query.projections,
+                                       context.valencer.query.populations);
+  }
+  // FIXME for cytoframes
   logger.verbose(`${context.valencer.results.frames.length} unique Frames retrieved from database in ${utils.getElapsedTime(startTime)}ms`);
   return next();
 }
--- middlewares/processors/lexUnits.js
+++ middlewares/processors/lexUnits.js
@@ -5,19 +5,50 @@ const logger = config.logger;
 
 function getLexUnitsWithLexUnitModel(LexUnit) {
   return async function getLexUnits(annotationSets, projections = {}, populations = []) {
-    const q = LexUnit.find({}, projections).where('_id')
+    const lexunits = LexUnit.find({}, projections).where('_id')
         .in(annotationSets.map(annoset => annoset.lexUnit));
-    return populations.reduce((query, p) => query.populate(p), q);
+    return populations.reduce((query, p) => query.populate(p), lexunits);
+  };
+}
+
+function getCytoEdges(lexunits) {
+  const edges = [];
+  for (let i = 1; i < lexunits.length; i += 1) {
+    if (lexunits[0].frame === lexunits[i].frame) {
+      edges.push({ data: { source: lexunits[0]._id, target: lexunits[i]._id } });
+    }
+  }
+  if (lexunits.length > 1) {
+    const slice = lexunits.slice(1);
+    edges.push(...getCytoEdges(slice));
+  }
+  return edges;
+}
+
+function getCytoLexUnitsWithLexUnitModel(LexUnit) {
+  return async function getCytoLexUnits(annotationSets) {
+    const lexunits = await LexUnit.find({}, 'name frame').where('_id')
+        .in(annotationSets.map(annoset => annoset.lexUnit));
+    const cytolexunits = lexunits.map(lexunit => ({
+      data: { id: lexunit._id, name: lexunit.name, frame: lexunit.frame } }));
+    const edges = getCytoEdges(lexunits);
+    return cytolexunits.concat(edges);
   };
 }
 
 async function getByAnnotationSets(context, next) {
   const startTime = utils.getStartTime();
   logger.info(`Querying for all LexUnits with a valence pattern matching: '${context.query.vp}'`);
-  context.valencer.results.lexUnits = await getLexUnitsWithLexUnitModel(
-          context.valencer.models.LexUnit)(context.valencer.results.annotationSets,
-                                           context.valencer.query.projections,
-                                           context.valencer.query.populations);
+  if (context.query.format === 'cytoscape') {
+    context.valencer.results.lexUnits = await getCytoLexUnitsWithLexUnitModel(
+      context.valencer.models.LexUnit)(context.valencer.results.annotationSets);
+  } else {
+    context.valencer.results.lexUnits = await getLexUnitsWithLexUnitModel(
+            context.valencer.models.LexUnit)(context.valencer.results.annotationSets,
+                                             context.valencer.query.projections,
+                                             context.valencer.query.populations);
+  }
+  // FIXME for cytolexunits
   logger.verbose(`${context.valencer.results.lexUnits.length} unique LexUnits
     retrieved from database in ${utils.getElapsedTime(startTime)}ms`);
   return next();
