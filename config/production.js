f17e37ac7b890f683fa6702869abc0b45b5a2284
akb89 <akb@3azouz.net>
2018-01-18 14:39:05 +0100

    Updated config

--- config/production.js
+++ config/production.js
@@ -13,6 +13,7 @@ const config = {
         170: 'fn_en_170',
         '170_train_ft': 'fn_en_170_train_ft',
         '170_train_ft_ex': 'fn_en_170_train_ft_ex',
+        '170_dev': 'fn_en_170_dev',
       },
       ja: {
         100: 'fn_ja_100',
