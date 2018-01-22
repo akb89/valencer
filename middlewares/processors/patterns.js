a3fb70036ce486d0f7960b489802846c1ae60167
akb89 <akb@3azouz.net>
2018-01-22 10:20:17 +0100

    Added skip/limit support for patterns and valenceUnits

--- middlewares/processors/patterns.js
+++ middlewares/processors/patterns.js
@@ -4,22 +4,43 @@ const utils = require('../../utils/utils');
 const logger = config.logger;
 
 function getPatternsWithPatternModel(Pattern) {
-  return async function getPatterns(patternIDs, projections = {}, populations = []) {
-    const q = Pattern.find({}, projections).where('_id').in(patternIDs);
+  return async function getPatterns(filteredPatternsIDs, countMode = false,
+                                    projections = {}, populations = [],
+                                    skip, limit) {
+    if (countMode) {
+      return Pattern.find().select(projections).where('_id')
+        .in(filteredPatternsIDs)
+        .count();
+    }
+    const q = Pattern.find({}, projections).where('_id')
+      .in(filteredPatternsIDs)
+      .skip(skip)
+      .limit(limit);
     return populations.reduce((query, p) => query.populate(p), q);
   };
 }
 
 async function getFromIDs(context, next) {
   const startTime = utils.getStartTime();
-  logger.info(`Querying for all valence patterns matching: '${context.query.vp}'`);
+  logger.info(`Querying for all valence patterns matching: '${context.query.vp}' with skip = '${context.valencer.query.skip}' and limit = '${context.valencer.query.limit}'`);
   const patternModel = context.valencer.models.Pattern;
   const filteredPatternsIDs = context.valencer.results.tmp.filteredPatternsIDs;
-  context.valencer.results.patterns =
-    await getPatternsWithPatternModel(patternModel)(filteredPatternsIDs,
-                                                    context.valencer.query.projections,
-                                                    context.valencer.query.populations);
-  logger.verbose(`${context.valencer.results.patterns.length} unique Patterns retrieved from database in ${utils.getElapsedTime(startTime)}ms`);
+  const [count, results] = await Promise.all([
+    getPatternsWithPatternModel(patternModel)(filteredPatternsIDs, true),
+    getPatternsWithPatternModel(patternModel)(filteredPatternsIDs,
+                                              false,
+                                              context.valencer.query.projections,
+                                              context.valencer.query.populations,
+                                              context.valencer.query.skip,
+                                              context.valencer.query.limit),
+  ]);
+  context.set({
+    'Total-Count': count,
+    Skip: context.valencer.query.skip,
+    Limit: context.valencer.query.limit,
+  });
+  context.valencer.results.patterns = results;
+  logger.verbose(`${results.length} unique Patterns out of ${count} retrieved from database in ${utils.getElapsedTime(startTime)}ms`);
   return next();
 }
 
