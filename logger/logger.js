7c98b30a9cbb63ef9f646c0a2f03041c55275532
akb89 <akb@3azouz.net>
2018-08-21 11:18:51 -0600

    Updated winston dependency

--- logger/logger.js
+++ logger/logger.js
@@ -1,35 +1,43 @@
 const winston = require('winston');
 
-const warn = new (winston.Logger)({
+const myFormat = winston.format.combine(winston.format.timestamp(),
+                                        winston.format.colorize(),
+                                        winston.format.printf(mess => `[${mess.timestamp}] ${mess.level}: ${mess.message}`));
+
+const warn = winston.createLogger({
   transports: [
     new (winston.transports.Console)({
+      format: myFormat,
       level: 'warn',
       colorize: true,
     }),
   ],
 });
 
-const info = new (winston.Logger)({
+const info = winston.createLogger({
   transports: [
     new (winston.transports.Console)({
+      format: myFormat,
       level: 'info',
       colorize: true,
     }),
   ],
 });
 
-const verbose = new (winston.Logger)({
+const verbose = winston.createLogger({
   transports: [
     new (winston.transports.Console)({
+      format: myFormat,
       level: 'verbose',
       colorize: true,
     }),
   ],
 });
 
-const debug = new (winston.Logger)({
+const debug = winston.createLogger({
   transports: [
     new (winston.transports.Console)({
+      format: myFormat,
       level: 'debug',
       colorize: true,
     }),
